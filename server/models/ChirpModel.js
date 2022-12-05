be3a486749a206085b67e91f6d374a0f63b5eaa2
emilalg <Emil.Algars@metropolia.fi>
2022-12-05 15:49:38 +0200

    -user model

--- server/models/ChirpModel.js
+++ server/models/ChirpModel.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
-
 const Schema = mongoose.Schema
+
 const chirpSchema = new Schema(
   {
     username: {
       type: String,
       required: true,
+      maxLength: 16,
     },
     content: {
       type: String,
@@ -20,4 +21,41 @@ const chirpSchema = new Schema(
   { timestamps: true }
 )
 
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      maxLength: 16,
+    },
+    passHash: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    pfpUrl: {
+      type: String,
+      required: true,
+      maxLength: 100,
+    },
+    verified: {
+      type: Boolean,
+      required: true,
+    },
+    followers: [{
+      username: {type: String, maxLength: 16,}
+    }],
+    follows: [{
+      username: {type: String, maxLength: 16,}
+    }],
+  },
+  { timestamps: true }
+)
+
+
 module.exports = mongoose.model('Chirp', chirpSchema)
+module.exports = mongoose.model('User', userSchema)
