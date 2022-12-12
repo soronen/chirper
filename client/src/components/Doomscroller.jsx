3eb08d5563256fd9c0a52a6312f722300025995c
Eetu Soronen <eetu.soronen@hotmail.com>
2022-12-12 16:44:17 +0200

    it works on my machine

--- client/src/components/Doomscroller.jsx
+++ client/src/components/Doomscroller.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { useAuthContext } from '../hooks/useAuthContext'
 import jwtDecode from 'jwt-decode'
 
 import Chirp from './Chirp'
 
-const apiUrl = process.env.REACT_APP_API_URL
-console.log(apiUrl)
 
 // robot wrote this 🤖
 const Doomscroller = ({ items, setItems }) => {
   const [page, setPage] = React.useState(0)
   const [hasMore, setHasMore] = React.useState(true)
-
+  
   const { user } = useAuthContext()
   let username = "null"
   try {
@@ -23,6 +21,29 @@ const Doomscroller = ({ items, setItems }) => {
   } catch (error) {
     console.log(error)
   }
+  const apiUrl = process.env.REACT_APP_API_URL
+  console.log(apiUrl)
+  
+  
+  useEffect(() => {
+    // increment the page number
+    const nextPage = page + 1
+    setPage(nextPage)
+
+    // make a GET request to the server to fetch the next page of items
+    fetch(apiUrl + `/posts/getAll?page=${nextPage}`)
+      .then((res) => res.json())
+      .then((newItems) => {
+        // add the new items to the list
+        setItems([...items, ...newItems])
+
+        // set hasMore to false when there are no more items to fetch
+        if (newItems.length === 0) {
+          setHasMore(false)
+        }
+      })
+    console.log('items', items)
+  }, [])
 
   const fetchMoreData = () => {
     // increment the page number
