9c04a941e363c5aa944a6c15172c727577dca8e9
Eetu Soronen <eetu.soronen@hotmail.com>
2022-12-14 14:32:18 +0200

    not logged in message when going to profile unlogged

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import ProfileInfo from '../components/ProfileInfo'
+import { useAuthContext } from '../hooks/useAuthContext'
+
 function Profile() {
-  return <ProfileInfo></ProfileInfo>
+  const { user } = useAuthContext()
+
+  return (
+    <>
+    {user ? (<ProfileInfo></ProfileInfo>) : <h1>Not logged in</h1>}
+    </>
+  )
 }
 
 export default Profile
