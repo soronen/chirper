05291c17b9d2d943894f8be4f38f00ff72fac484
Eetu Soronen <eetu.soronen@hotmail.com>
2022-12-14 14:22:09 +0200

    Revert "profile button hidden when not logged in"
    This reverts commit 53f3a07de466f29b51e17229f4fbd3634931a9c3.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,13 +10,13 @@ import { useLogout } from '../hooks/useLogout'
 
 function Navbar() {
   const { logout } = useLogout()
-
+  
   const { user } = useAuthContext()
-  let username = 'null'
+  let username = "null"
   try {
     if (user !== null && user.jtw !== null) {
       username = jwtDecode(user.jwt).username
-      console.log('username: ', username)
+      console.log('username: ', username);
     }
   } catch (error) {
     console.log(error)
@@ -37,43 +37,50 @@ function Navbar() {
     <header>
       <div className='bg-indigo-500 text-white h-14 text-lg flex flex-row justify-between items-center px-4'>
         <div className='flex gap-4'>
+
+
+
           <Link
             to='/'
             className='rounded-lg p-2 hover:bg-violet-400'
             style={{
               backgroundColor: location.pathname === '/' ? '#a78bfa' : '',
-            }}>
+            }}
+          >
             <h1>Home</h1>
           </Link>
-          {user && (
-            <Link
-              to='/profile'
-              className='rounded-lg p-2 hover:bg-violet-400'
-              style={{
-                backgroundColor: location.pathname === '/profile' ? '#a78bfa' : '',
-              }}>
-              <h1>Profile</h1>
-            </Link>
-          )}
+
+          <Link
+            to='/profile'
+            className='rounded-lg p-2 hover:bg-violet-400'
+            style={{
+              backgroundColor:
+                location.pathname === '/profile' ? '#a78bfa' : '',
+            }}
+          >
+            <h1>Profile</h1>
+          </Link>
 
           <Link
             to='/about'
             className='rounded-lg p-2 hover:bg-violet-400'
             style={{
-              backgroundColor: location.pathname === '/about' ? '#a78bfa' : '',
-            }}>
+              backgroundColor:
+                location.pathname === '/about' ? '#a78bfa' : '',
+            }}
+          >
             <h1>About</h1>
           </Link>
+
         </div>
         <div className='flex flex-row'>
           {user ? (
             <>
-              <h1 className='rounded-lg p-2 mr-10'>
-                Logged in as <strong>{username}</strong>
-              </h1>
+              <h1 className='rounded-lg p-2 mr-10' >Logged in as <strong>{username}</strong></h1>
               <button
                 onClick={toggleLogOut}
-                className='rounded-lg p-2 hover:bg-violet-400'>
+                className='rounded-lg p-2 hover:bg-violet-400'
+              >
                 Log out
               </button>
             </>
